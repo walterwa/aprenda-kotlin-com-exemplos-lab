e5353c3217b0300e56b6327d1c802096ccdf070b
Walter Andrade <65465090+walterwa@users.noreply.github.com>
2022-12-21 20:59:00 -0300

    resolução desafio kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,63 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val identidade: Long)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    infix fun matricular(usuario: Usuario) { inscritos.add(usuario) }
+    
+    fun matricular(vararg usuarios: Usuario) {
+        for (usuario in usuarios)
+       		inscritos.add(usuario)
+    }
+    
+    fun gradeConteudos(){ 
+        for (conteudo in conteudos)
+        	println(conteudo.nome)
+    }
+    
+    fun listaInscritos() { 
+        for (inscrito in inscritos)
+        	println("${inscrito.nome} ${inscrito.identidade}")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+        
+    //criando usuários
+    val arnaldo = Usuario("Alberto", 123)
+    val tereza = Usuario("Tereza", 987)
+    val celio = Usuario("Célio", 100)
+    
+    
+   
+    // adicionando o conteúdo da disciplina da formação
+    val listaConteudosAntropologia:List<ConteudoEducacional> = listOf(
+        ConteudoEducacional("Introdução à Teoria Antropológica"),
+        ConteudoEducacional("Teoria Antropológica II"),
+        ConteudoEducacional("História do Brasil"),
+        ConteudoEducacional("Cultura Popular")
+    )
+    
+    //criando a formação
+    val antropologia = Formacao("Antropologia", listaConteudosAntropologia, Nivel.BASICO)
+    
+    //matriculando
+        antropologia.matricular(arnaldo)
+        antropologia.matricular(tereza)
+        antropologia.matricular(celio)
+   
+    
+    //print do resultado final com os testes
+    println("*-**-* Formação: ${antropologia.nome} *-**-**-*")
+    println("Conteúdos: \n")
+    antropologia.gradeConteudos()
+    println(" ")
+    println("Lista de inscritos: \nNome | Identidade: \n")
+    antropologia.listaInscritos()
 }
